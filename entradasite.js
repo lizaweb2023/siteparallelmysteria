b8ad8eb0b9f4f384d1416dddef5656cca954c52a
lizaweb2023 <liz.no.ar2015@gmail.com>
2023-06-26 16:43:34 -0300

    tentatr arrumar

--- entradasite.js
+++ entradasite.js
@@ -1,7 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Login</title>
+
   <script>
     function fazerLogin() {
       // Obter os valores de entrada do formulário de login
@@ -17,18 +14,3 @@
       }
     }
   </script>
-</head>
-<body>
-  <h1>Login</h1>
-  
-  <form>
-    <label for="email">E-mail:</label>
-    <input type="text" id="email" required><br><br>
-    
-    <label for="senha">Senha:</label>
-    <input type="password" id="senha" required><br><br>
-    
-    <input type="button" value="Entrar" onclick="fazerLogin()">
-  </form>
-</body>
-</html>
---
+++ segundaentrada.js
@@ -0,0 +1,16 @@
+function fazerLogin (){
+        //valores de entrada do formulario
+        let email = document.getElementById("email").value;
+
+        let senha = document.getElementById ("senha").value;
+
+        // como validar o email e senha 
+
+        if (email === "email" && senha === "senha") {
+            // redirecionar para a pagina principal
+            window.location.href = "homedositeparallel.html";
+
+        } else {
+                alert("sua entrada não foi autorizada")
+            }
+}
