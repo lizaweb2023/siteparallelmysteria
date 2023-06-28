df8c5c08d03fde9618d826a27ee0144d477d373d
lizaweb2023 <liz.no.ar2015@gmail.com>
2023-06-28 16:11:00 -0300

    modificações

--- segundaentrada.js
+++ segundaentrada.js
@@ -1,19 +1,36 @@
-document.getElementById("enviarBtn").addEventListener("click", fazerLogin);
+document.getElementById("enviarBtn").addEventListener("click", fazerLogin); 
+    //function fazerLogin () {
+    //valores de entrada do formulario
 
-     function fazerLogin (){
-        //valores de entrada do formulario
-        let email = document.getElementById("email").value;
+  function fazerLogin() {
+ 
+  let email = document.getElementById("email").value;
 
-        let senha = document.getElementById ("senha").value;
+  let senha = document.getElementById ("senha").value;
 
-        // como validar o email e senha 
+    // como validar o email e senha 
 
-        if (email === "email" && senha === "senha") {
-            // redirecionar para a pagina principal
-            window.location.href = "./homedositeparallel.html";
+ if (email === "email" && senha === "senha") {
+        // redirecionar para a pagina principal
+       window.location.href = "./homedositeparallel.html";
 
-        } else {
-                alert("sua entrada não está autorizada");
-
-            }
+  } else {
+          alert("sua entrada não está autorizada");
+    }
 };
+
+//document.getElementById("enviarBtn").addEventListener("submit", function(event) {
+            //event.preventDefault(); // Evita o envio do formulário
+
+            //var email = document.getElementById("email").value;
+            //var senha = document.getElementById("senha").value;
+
+            // Aqui você pode realizar validações ou enviar os dados do formulário para um servidor
+
+            // Exemplo de validação simples
+           // if (email === "usuario@example.com" && senha === "123456") {
+           //     alert("Bem vindo aventureiro");
+           // } else {
+             //   alert("você não está autorizado.");
+          //  }
+       // }); 
